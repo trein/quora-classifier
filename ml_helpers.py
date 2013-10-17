e6bfb9d9783f7aedc0f1819e1ced0306984e0e0a
Guilherme M. Trein <guitrein@gmail.com>
2013-10-17 16:12:45 -0400

    Improve normalization method

--- ml_helpers.py
+++ ml_helpers.py
@@ -14,7 +14,7 @@ def extract(file):
     targets = []
 
     for line in traindata:
-        formatted_line = line.replace("\n", "")
+        formatted_line = line.strip("\n")
         target_i = formatted_line.split(" ")[1]
         feature_i = re.sub(r"(\d+):", "", formatted_line).split(" ")[2:]
 
@@ -22,17 +22,19 @@ def extract(file):
         features.append(feature_i)
 
     matrix_features = np.array(features).astype(np.float)
-
-    max_features = matrix_features.max(axis = 0)
-    max_features = (max_features + (max_features == 0))
-
-    normal_matrix_features = matrix_features / max_features
+    normal_matrix_features = normalize_features(matrix_features)
     vector_targets = np.array(targets).astype(np.int)
 
     # print normal_matrix_features
     # print "Max", max_features
 
     return (normal_matrix_features, vector_targets)
+
+def normalize_features(matrix_features):
+    max_features = matrix_features.max(axis = 0)
+    max_features = (max_features + (max_features == 0))
+
+    return matrix_features / max_features
 
 def accuracy(targets_hat, targets):
     return (1.0 * (targets_hat == targets)).sum(0) / targets.shape
